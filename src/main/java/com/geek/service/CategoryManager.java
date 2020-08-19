a4b93ab501bc60cde98dae556bc0628a3850d7b3
lyfGeek。 <YifanLiGeek@gmail.com>
2020-08-19 19:52:59 +0800

    Geek。

---
+++ src/main/java/com/geek/service/CategoryManager.java
@@ -0,0 +1,18 @@
+package com.geek.service;
+
+import com.geek.model.Category;
+
+import java.util.List;
+
+public interface CategoryManager {
+    public List<Category> getAllCategories();
+
+    public Category getCategoryById(int cid);
+
+    public int addCategory(String cname);
+
+    public int updateCategoryById(int cid, String cname);
+
+    public int deleteCategoryById(int cid);
+}
+
